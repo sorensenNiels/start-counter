abff84e54d82d0b7851daa6cc4bf4d150fd4e1de
Niels P. Sorensen <niels.p.sorensen@softdesign.dk>
2025-06-18 14:47:32 +0200

    Enabled shadcn

--- src/routes/counter.tsx
+++ src/routes/counter.tsx
@@ -1,26 +1,30 @@
-import { useRouter, createFileRoute } from '@tanstack/react-router'
-import { getCount, updateCount } from '~/fn/counter'
+import { useRouter, createFileRoute } from "@tanstack/react-router";
+import { Button } from "~/components/ui/button";
+import { getCount, updateCount } from "~/fn/counter";
 
-const filePath = 'count.txt'
+const filePath = "count.txt";
 
-export const Route = createFileRoute('/counter')({
+export const Route = createFileRoute("/counter")({
   component: Home,
   loader: async () => await getCount(),
-})
+});
 
 function Home() {
-  const router = useRouter()
-  const state = Route.useLoaderData()
+  const router = useRouter();
+  const state = Route.useLoaderData();
 
   return (
-    <button
-      onClick={() => {
-        updateCount({ data: 1 }).then(() => {
-          router.invalidate()
-        })
-      }}
-    >
-      Add 1 to {state}?
-    </button>
-  )
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Button
+        variant="default"
+        onClick={() => {
+          updateCount({ data: 1 }).then(() => {
+            router.invalidate();
+          });
+        }}
+      >
+        Add 1 to {state}?
+      </Button>
+    </div>
+  );
 }
